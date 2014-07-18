16e7accd2859a64b8cbefc606e5514b151be1519
lrochette <lrochette@electric-cloud.com>
2014-07-17 23:02:41 -0700

    EC-Admin v1.10.2.73: Appended the release comment to plugin description when it is present

--- src/project/procedures/projectAsCode/steps/create_CHANGELOG.pl
+++ src/project/procedures/projectAsCode/steps/create_CHANGELOG.pl
@@ -33,7 +33,7 @@ if (-f $file) {
     printf("Content of CHANGELOG:\n");
     print @changeLogContent;
 }
-unshift (@changeLogContent,"$[/timestamp YYYY-MM-DD] $userFullName <$userEmail>\n  * $comment\n\n");
+unshift (@changeLogContent,"$[/timestamp YYYY-MM-dd] $userFullName <$userEmail>\n  * $comment\n\n");
 
 unless (open FILE, '>'.$file) {
         printf("File: %s\n", $file);
