7be0ff4d86d6c97990be89cd615b6ccd395d97e8
lrochette <lrochette@electric-cloud.com>
2014-07-26 03:30:38 -0700

    EC-Admin v1.11.7.113:

--- src/project/procedures/projectAsCode/steps/promotePlugin.pl
+++ src/project/procedures/projectAsCode/steps/promotePlugin.pl
@@ -5,5 +5,5 @@ my $version="$[/myJob/Version]";
 
 printf("Promoting plugin '$plugin-$version'\n");
 $ec->promotePlugin("$plugin-$version");
-$ec->setProperty("summary", "<html><a href=\"https://$[/server/hostName]/commander/link/projectDetails/plugins/$plugin-$version\">$plugin-$version</a> promoted</html>");
+$ec->setProperty("summary", "<html>&#160;&#160;&#160;<a href=\"https://$[/server/hostName]/commander/link/projectDetails/plugins/$plugin-$version\">$plugin-$version</a> promoted</html>");
 
