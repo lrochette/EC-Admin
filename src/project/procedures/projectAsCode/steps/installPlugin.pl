556e1893d5a5c809f61bad630680dd52e82aac00
lrochette <lrochette@electric-cloud.com>
2014-07-30 09:28:47 -0700

    EC-Admin v1.11.8.127: Fixed summary link to the new plugin version in projectAsCode.

--- src/project/procedures/projectAsCode/steps/installPlugin.pl
+++ src/project/procedures/projectAsCode/steps/installPlugin.pl
@@ -7,5 +7,5 @@ my $version="$[/myJob/Version]";
 
 printf("Installing plugin '$plugin-$version'\n");
 $ec->installPlugin("./$plugin.jar");
-$ec->setProperty("summary", "<html>&#160;&#160;&#160;<a href=\"https://$[/server/hostName]/commander/link/projectDetails/plugins/$plugin-$version\">$plugin-$version</a> installed</html>");
+$ec->setProperty("summary", "<html>&#160;&#160;&#160;<a href=\"/commander/link/projectDetails/plugins/$plugin-$version\">$plugin-$version</a> installed</html>");
 
--- src/project/procedures/projectAsCode/steps/promotePlugin.pl
+++ src/project/procedures/projectAsCode/steps/promotePlugin.pl
@@ -5,5 +5,8 @@ my $version="$[/myJob/Version]";
 
 printf("Promoting plugin '$plugin-$version'\n");
 $ec->promotePlugin("$plugin-$version");
-$ec->setProperty("summary", "<html>&#160;&#160;&#160;<a href=\"https://$[/server/hostName]/commander/link/projectDetails/plugins/$plugin-$version\">$plugin-$version</a> promoted</html>");
+
+$ec->setProperty("summary", "<html>&#160;&#160;&#160;<a href=\"/commander/link/projectDetails/plugins/$plugin-$version\">$plugin-$version</a> promoted</html>");
+$ec->setProperty("/myJob/report-urls/$plugin-$version",
+    "/commander/link/projectDetails/projects/$plugin-$version");
 
