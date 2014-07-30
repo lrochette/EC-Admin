eab5d5c55890ee7227a7a81280113eabb5d2de0a
lrochette <lrochette@electric-cloud.com>
2014-07-30 09:21:43 -0700

    EC-Admin v1.11.8.126: Fixed Help for Directory parameter in projectAsCode

--- src/project/procedures/projectAsCode/steps/buildPlugin.pl
+++ src/project/procedures/projectAsCode/steps/buildPlugin.pl
@@ -5,14 +5,8 @@ use File::Copy qw(move);
 my $pluginName="$[/myJob/pluginName]";
 my $Version="$[/myJob/Version]";
 
-if (my $ant_path=which('ant')) {
-  my @args=("ant build");
-  system(@args) == 0 or die("system @args failed: $?");
-} else {
-  printf("ant not found in the path\n");
-  $ec->setProperty("summary", "No ant executable in the PATH");
-  exit(1);
-}
+my @args=("\"$[SDKpath]/tools/ant/bin/ant\" build");
+system(@args) == 0 or die("system @args failed: $?");
 
 $ec->setProperty("/myJob/report-urls/$pluginName-$Version.jar", "file:/$[/myWorkspace/agentUnixPath]/$[/myJob/directoryName]/$pluginName-$Version.jar");
 
