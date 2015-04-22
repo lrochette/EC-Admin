f948a188b9cca134c5f2184344524a8f93b45197
lrochette <lrochette@electric-cloud.com>
2015-04-22 14:38:37 -0700

    EC-Admin v2.6.0.260: Fixed changeBannerColor for 5.4

--- src/project/procedures/changeBannerColor/steps/updateCSS.pl
+++ src/project/procedures/changeBannerColor/steps/updateCSS.pl
@@ -1,26 +1,33 @@
 $[/myProject/scripts/perlHeaderJSON]
-
+use File::Copy;
+    
 my $color=lc("$[color]");
 $color="#000000" if ($color eq "default");
 
-my $cssFile= "$env{COMMANDER_HOME}/apache/htdocs/commander/lib/styles/StdFrame.css";
+my $cssFile= "$ENV{COMMANDER_HOME}/apache/htdocs/commander/lib/styles/StdFrame.css";
+copy($cssFile,"${cssFile}_$[/timestamp YYYY-MM-dd]") or die "Copy failed: $!";
 
-open(my FH, "< $cssFile") || die ("Cannot open $cssFile\n");
-my @lines=<FH>;
-close(FH);
+open(my $FH, "< $cssFile") || die ("Cannot open $cssFile\n");
+my @lines=<$FH>;
+close($FH);
 
 my @out=();
 my $foundBlock =0;
 
 foreach my $line (@lines) {
 	if ($line =~ /\.frame_bannerTop/) {
-    	push (@out, "/* modified by EC-Admin on $[/timestamp YYYY-MM-dd]\n");
+    	push (@out, "/* modified by EC-Admin on $[/timestamp YYYY-MM-dd] */\n");
         push (@out, $line);
-        $foundBlock =1;
-    } else if ($line =~ /background-color/) {
-    	push(@out, "    background-color: $color\n");
-        $foundBlock =0;
+        $foundBlock = 1;
+    } elsif ($foundBlock && ($line =~ /background-color/)) {
+    	push(@out, "    background-color: $color;\n");
+        $foundBlock = 0;
     } else {
         push (@out, $line);
     }
 }
+
+open(my $FH, "> $cssFile") || die ("Cannot open $cssFile\n");
+print $FH @out;
+close($FH);
+
