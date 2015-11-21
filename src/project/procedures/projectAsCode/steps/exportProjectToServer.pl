9451f379cd7581972e9a84f02106718eff4a1168
lrochette <lrochette@electric-cloud.com>
2015-11-20 21:18:58 -0800

    EC-Admin v2.16.0.317: add option to enable change tracking on the created plugin

--- src/project/procedures/projectAsCode/steps/exportProjectToServer.pl
+++ src/project/procedures/projectAsCode/steps/exportProjectToServer.pl
@@ -4,8 +4,8 @@
 #
 #############################################################################
 
-$[/myProject/scripts/perlHeader]
-use XML::LibXML;
+$[/myProject/scripts/perlHeaderJSON]
+#use XML::LibXML;
 use File::Path;
 
 #############################################################################
@@ -14,6 +14,7 @@ use File::Path;
 #
 #############################################################################
 my $prj="$[Project]";		# name of the project to transform
+my $changeTracking="$[changeTracking]";
 
 #############################################################################
 #
@@ -25,18 +26,32 @@ my $manifest = ""; 	# manifest.pl file content
 
 # Use the logs directory for the temporary export file (we know it's writable by the
 # server since it writes its logs there).
-my $exportFile = $ec->getProperty("/server/Electric Cloud/dataDirectory")
-        		->findvalue("//value")->value() . "/logs/project.xml";
+my $exportFile = getP("/server/Electric Cloud/dataDirectory") . "/logs/project.xml";
 
-
-#Create the export file
-# use ElectricCommander;
-$ec->export($exportFile,{
+# check that we are running version 5.x or later
+my %exportOptions=(
 	 path => "/projects/$prj",
 	 excludeJobs => 'true',
 	 relocatable => 'true',
 	 withNotifiers => 'true'
-});
+);
+
+
+my $version=getVersion();
+if (compareVersion($version, "5.0") > 0) {
+  if ($changeTracking) {
+    $exportOptions{disableProjectTracking}="false";
+  } else {
+    $exportOptions{disableProjectTracking}="true";
+  }
+}
+
+
+#Create the export file
+# use ElectricCommander;
+$ec->export($exportFile, \%exportOptions);
+
+$[/myProject/scripts/perlLibJSON]
 
 
 
