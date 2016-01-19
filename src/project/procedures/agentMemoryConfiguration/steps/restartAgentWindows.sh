82c596c008f123594069a00ffb8560700f4c1d91
lrochette <lrochette@electric-cloud.com>
2016-01-19 13:37:49 -0800

    EC-Admin v2.20.1.403:

---
+++ src/project/procedures/agentMemoryConfiguration/steps/restartAgentWindows.sh
@@ -0,0 +1,12 @@
+#############################################################################
+#
+#  restartAgent -- Script to possibly restart an agent.
+#  Copyright 2016 Electric-Cloud Inc.
+#
+#############################################################################
+
+if ($osIsWindows) {
+  $ec->setProperty("summary", "Rebooting the agent");
+  system("shutdown /r /c \"ELectricFlow: modification of the Java Heap memory settings\" /t 15")
+}
+
