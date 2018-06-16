41f92489d44e8f1fae7a366e5790a631c519f99f
Laurent Rochette <lrochette@eletric-cloud.com>
2018-06-15 19:04:08 -0500

    Convert subPM-deploymentSize

---
+++ dsl/procedures/subPM-deploymentSize/steps/managedResources.pl
@@ -0,0 +1,25 @@
+$[/myProject/scripts/perlHeaderJSON]
+
+################################################################################
+#
+# GLobal variables
+#
+################################################################################
+my $SMALL=100;
+my $LARGE=1000;
+
+my ($ok, $json)=InvokeCommander("SuppressLog", 'countObjects', 'resource');
+my $nbResources=$json->{responses}->[0]->{count};
+
+my $size;
+if ($nbResources < $SMALL) {
+	$size="SMALL";
+} elsif ($nbResources > $LARGE) {
+	$size="LARGE";
+} else {
+	$size="MEDIUM";
+}
+$ec->setProperty("summary", "$size: $nbResources resources");
+$ec->setProperty("/myJob/PM-numberOfResources", $nbResources);
+
+$[/myProject/scripts/perlLibJSON]
