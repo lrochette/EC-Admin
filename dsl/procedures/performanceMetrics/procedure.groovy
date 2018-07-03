57b3cefe6e0c17be85ab10745e94613970917642
Laurent Rochette <lrochette@eletric-cloud.com>
2018-07-03 10:01:42 -0500

    Split core metrics in its own sub-procedure for easier testing

--- dsl/procedures/performanceMetrics/procedure.groovy
+++ dsl/procedures/performanceMetrics/procedure.groovy
@@ -3,6 +3,7 @@ import java.io.File
 /*
 Changelog:
 --------------
+ 07-03-2018  lrochette   Split core metrics in its own sub-procedure for easier testing
  06-15-2018  lrochette    Transform to PluginWizard (DSL) format
  04-09-2015  lrochette    Replaced some findObjects by countObjects
  08-09-2013  lrochette    Fixed bug on Free available RAM with European numbers
@@ -19,70 +20,14 @@ procedure procName,
   description: 'Performance diagnostics for EC server and agents',
   jobNameTemplate: '$[/plugins[EC-Admin]/project/scripts/jobTemplate]',
 {
-  step 'serverMemoryAmount',
-    description: '''Check the amount of memory for the EC server
-Default values:
-Bad: < 8 GB
-Good: Between 8 and 12 GB
-Best: More than 12 GB''',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/serverMemoryAmount.pl").text,
-    resourceName: 'local',
-    shell: 'ec-perl'
-
-  step 'freeMemory',
-    description: '''Check the amount of  free memory for the EC server
-Information only''',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/freeMemory.pl").text,
-    resourceName: 'local',
-    shell: 'ec-perl'
-
-  step 'diskSpaceAvailable',
-    description: '''Check the amount of  available disk space for the EC server
-Information only''',
-    resourceName: 'local',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/diskSpaceAvailable.pl").text,
-    shell: 'ec-perl'
-
-  step 'javaHeap',
-    description: '''Report Java size information''',
-    resourceName: 'local',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/javaHeap.pl").text,
-    shell: 'ec-perl'
-
-  step 'numberOfCores',
-    description: '''Check the number of cores for the EC server
-Default values:
-Bad: < 4
-Good: Between 4 and 6
-Best: More than 6''',
-    resourceName: 'local',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/numberOfCores.pl").text,
-    shell: 'ec-perl'
-
-  step 'serverCpuSpeed',
-    description: '''Check the CPU speed for the EC server
-Default values:
-Bad: < 1.5 GHz
-Good: Between 1.5 and 2 GHz
-Best: More than 2 GHz''',
-    resourceName: 'local',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/serverCpuSpeed.pl").text,
-    shell: 'ec-perl'
-
   step 'licenseUsage',
     description: 'Display information about license usage.',
     command: new File(pluginDir, "dsl/procedures/$procName/steps/licenseUsage.pl").text,
     shell: 'ec-perl'
 
-  step 'databaseType',
-    description: '''Check the Database type for the EC server
-Default values:
-Bad: H2 or mariadb
-Good: mysql
-Best: mssql or oracle''',
-    resourceName: 'local',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/databaseType.pl").text,
-    shell: 'ec-perl'
+  step 'core',
+    subprocedure: 'subPM-core',
+    subproject: '$[/myProject/projectName]'
 
   step 'deploymentSize',
     subprocedure: 'subPM-deploymentSize',
