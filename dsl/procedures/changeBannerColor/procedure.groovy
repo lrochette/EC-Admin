ed420eb1cacb4ed4a0780aeef282f6ed15eb5a8e
Laurent Rochette <lrochette@eletric-cloud.com>
2018-08-27 21:42:46 -0500

    Fix #84 Allow banner color change on deploy side as well

--- dsl/procedures/changeBannerColor/procedure.groovy
+++ dsl/procedures/changeBannerColor/procedure.groovy
@@ -6,16 +6,32 @@ procedure procName,
   description: 'A procedure to change the color of the banner to easily identify your multiple servers (DEV vs. PROD)',
   jobNameTemplate: '$[/plugins[EC-Admin]/project/scripts/jobTemplate]',
 {
-  step 'updateCSS',
-    command: new File(pluginDir, "dsl/procedures/$procName/steps/updateCSS.pl").text,
+  step 'updateCSSPlatform',
+    command: new File(pluginDir, "dsl/procedures/$procName/steps/updateCSSPlatform.pl").text,
+    shell: 'ec-perl',
+    resourceName: '$[webResource]'
+  step 'updateCSSDeploy',
+    command: new File(pluginDir, "dsl/procedures/$procName/steps/updateCSSDeploy.pl").text,
     shell: 'ec-perl',
     resourceName: '$[webResource]'
 
-  step 'copyLogoFile',
+  step 'copyLogoFilePlatform',
     subprocedure: 'Remote Copy - Native',
     subproject: '/plugins/EC-FileOps/project',
     actualParameter: [
        destinationFile: '$[/server/Electric Cloud/installDirectory]/apache/htdocs/commander/images/logo.gif',
+      destinationResourceName: '$[webResource]',
+      destinationWorkspaceName: 'default',
+      sourceFile: '$[/javascript ("$[logoFile]" == "")? "$[/server/settings/pluginsDirectory]/$[/myProject]/htdocs/$[logo]" : "$[logoFile]" ;]',
+      sourceResourceName: 'local',
+      sourceWorkspaceName: 'default'
+    ]
+
+  step 'copyLogoFileDeploy',
+    subprocedure: 'Remote Copy - Native',
+    subproject: '/plugins/EC-FileOps/project',
+    actualParameter: [
+      destinationFile: '$[/server/Electric Cloud/installDirectory]/apache/htdocs/flow/public/app/assets/img/logo.png',
       destinationResourceName: '$[webResource]',
       destinationWorkspaceName: 'default',
       sourceFile: '$[/javascript ("$[logoFile]" == "")? "$[/server/settings/pluginsDirectory]/$[/myProject]/htdocs/$[logo]" : "$[logoFile]" ;]',
--- ecpluginbuilder.pl
+++ ecpluginbuilder.pl
@@ -14,7 +14,7 @@ my $ec = new ElectricCommander->new();
 
 my $epb="../ecpluginbuilder";
 
-my $pluginVersion = "3.1.2";
+my $pluginVersion = "3.1.3";
 my $pluginKey = "EC-Admin";
 
 GetOptions ("version=s" => \$pluginVersion)
