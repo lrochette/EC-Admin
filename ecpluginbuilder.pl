bcd83b667369ffb90fcc32362da76f4bff317b78
Laurent Rochette <lrochette@eletric-cloud.com>
2019-01-18 13:39:13 -0500

    Fix #156 issue with getP since 8.5

--- ecpluginbuilder.pl
+++ ecpluginbuilder.pl
@@ -14,7 +14,7 @@ my $ec = new ElectricCommander->new({timeout => 600});
 
 my $epb="../ecpluginbuilder";
 
-my $pluginVersion = "3.2.2";
+my $pluginVersion = "3.2.3";
 my $pluginKey = "EC-Admin";
 
 GetOptions ("version=s" => \$pluginVersion)
